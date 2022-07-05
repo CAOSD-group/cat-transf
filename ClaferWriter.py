c9aa030dc77dab71670216f68c994ad88aeef591
Adriana <anhe2p9@uma.es>
2022-07-05 10:50:34 +0200

    Transformation to Clafer

--- ClaferWriter.py
+++ ClaferWriter.py
@@ -1,5 +1,6 @@
-from pyexpat import features
+from pyexpat import features, model
 import re
+from typing import List
 from famapy.core.transformations import ModelToText
 from famapy.core.models.ast import ASTOperation
 from famapy.metamodels.fm_metamodel.models import FeatureModel, Feature, Relation, Constraint
@@ -26,6 +27,8 @@ def fm_to_clafer(feature_model: FeatureModel) -> str:
 
     #result = read_features(feature_model.root, '', -1)
     result = read_features2(feature_model.root, None, 0)
+    for ctc in feature_model.get_constraints():
+       result += read_constraints(ctc)
     return result
 
 def read_features2(feature: Feature, parent: Feature, tab_count: int) -> str:
@@ -47,7 +50,7 @@ def read_features2(feature: Feature, parent: Feature, tab_count: int) -> str:
     tabs = '\t' * tab_count
     result += tabs
 
-    if group_type: #nos devuelve true si hay algo dentro y false si está vacío
+    if group_type:
         result += f'{group_type} '
 
     result += feature.name
@@ -56,113 +59,24 @@ def read_features2(feature: Feature, parent: Feature, tab_count: int) -> str:
         result += ' ?'
 
     result += '\n'
-    
-    #result = f'{tabs} {group_type} {feature.name} {opt}\n' #hemos dado formato de string a una variable con f-string LO QUITAMOS
     tab_count += 1
-
     for child in feature.get_children():
         result += read_features2(child, feature, tab_count)
     return result
 
-
-def serialize_relation(rel: Relation) -> str:
+def read_constraints(const: Constraint) -> str:
     result = ""
-
-    if rel.is_alternative():
-        result = "xor "
-    elif rel.is_optional():
-        result = str(rel.card_min) + ".." + str(rel.card_max) + " "
-    elif rel.is_or():
-        result = "or "
-    return result
-
-def read_constraints(self) -> str:
-    result = "constraints"
-    constraints = self.model.ctcs
-    for constraint in constraints:
-        constraint_text = self.serialize_constraint(constraint)
-        result = result + "\n\t" + constraint_text
-
+    constraint_text = serialize_constraint(const)
+    result = "\n" + constraint_text
     return result
 
 def serialize_constraint(ctc: Constraint) -> str:
     ctc = ctc.ast.pretty_str()
     ctc = re.sub(fr'\b{ASTOperation.NOT.value}\ \b', 'not', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.AND.value}\b', '&', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.OR.value}\b', '|', ctc)
+    ctc = re.sub(fr'\b{ASTOperation.AND.value}\b', '&&', ctc)
+    ctc = re.sub(fr'\b{ASTOperation.OR.value}\b', '||', ctc)
     ctc = re.sub(fr'\b{ASTOperation.IMPLIES.value}\b', '=>', ctc)
     ctc = re.sub(fr'\b{ASTOperation.EQUIVALENCE.value}\b', '<=>', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.REQUIRES.value}\b', 'requires', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.EXCLUDES.value}\b', 'excludes', ctc)
+    ctc = re.sub(fr'\b{ASTOperation.REQUIRES.value}\b', '=>', ctc)
+    ctc = re.sub(fr'\b{ASTOperation.EXCLUDES.value}\b', '=> not', ctc)
     return f'[{ctc}]'
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-def read_features(feature: Feature, result: str, tab_count: int) -> str:
-    tab_count = tab_count + 1
-    result = result + "\n" + tab_count * "\t" + feature.name
-    tab_count = tab_count + 1
-    for relation in feature.relations:
-        relation_name = serialize_relation(relation)
-        result = result + "\n" + tab_count * "\t" + relation_name
-        for feature_node in relation.children:
-            result = read_features(feature_node, result, tab_count)
-    return result
-
-def serialize_relation(rel: Relation) -> str:
-    result = ""
-
-    if rel.is_alternative():
-        result = "alternative"
-    elif rel.is_mandatory():
-        result = "mandatory"
-    elif rel.is_optional():
-        result = "optional"
-    elif rel.is_or():
-        result = "or"
-    else:
-        min_value = rel.card_min
-        max_value = rel.card_max
-        if min_value == max_value:
-            result = "[" + str(min) + "]"
-        else:
-            result = "[" + str(min) + ".." + str(max) + "]"
-
-    return result
-
-def read_constraints(self) -> str:
-    result = "constraints"
-    constraints = self.model.ctcs
-    for constraint in constraints:
-        constraint_text = self.serialize_constraint(constraint)
-        result = result + "\n\t" + constraint_text
-
-    return result
-
-def serialize_constraint(ctc: Constraint) -> str:
-    ctc = ctc.ast.pretty_str()
-    ctc = re.sub(fr'\b{ASTOperation.NOT.value}\ \b', '!', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.AND.value}\b', '&', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.OR.value}\b', '|', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.IMPLIES.value}\b', '=>', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.EQUIVALENCE.value}\b', '<=>', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.REQUIRES.value}\b', 'requires', ctc)
-    ctc = re.sub(fr'\b{ASTOperation.EXCLUDES.value}\b', 'excludes', ctc)
-    return ctc
-
-
-'''
-
-
-
