29c0dc129f64e292cc9caaf44265893c91ae3494
Adriana <anhe2p9@uma.es>
2022-07-15 15:42:51 +0200

    Cardinality reviewed and complex prop. log. cross-tree constraint started

--- main_refactoring.py
+++ main_refactoring.py
@@ -87,10 +87,9 @@ def get_case5() -> FeatureModel:
     f1 = Feature(name='F1', parent=feature_root)
     f2 = Feature(name='F2', parent=feature_root)
     f3 = Feature(name='F3', parent=feature_root)
-    f4 = Feature(name='F4', parent=feature_root)
 
     # Create relations
-    rel = Relation(feature_root, [f1, f2, f3, f4], 2, 4)  # cardinality
+    rel = Relation(feature_root, [f1, f2, f3], 2, 3)  # cardinality
 
     # Add relations to features
     feature_root.add_relation(rel)
@@ -125,72 +124,90 @@ def get_case6() -> FeatureModel:
 
 
 
-def factorial(n: int) -> int:
-    if n<0:
-        raise Exception(f'"{n}" must be greater than 0.')
-    return 1 if (n==1 or n==0) else n*factorial(n-1)
+def get_case7() -> FeatureModel:
+    # Create features
+    feature_root = Feature(name='Root')
 
-def combinatorics(card_min: int, card_max: int) -> int:
-    return factorial(card_max)/(factorial(card_max - card_min)*factorial(card_min))
+    # Create constraints
+    left = AST.create_binary_operation(ASTOperation.OR, Node('Format1'), Node('Routing')).root
+    ctc = AST.create_binary_operation(ASTOperation.IMPLIES, left, Node('5G')).root
 
-def iter_combinations(n: int, card_max: int) -> int:
-    if n < card_max:
-        n += 1
-    return n
-
-def set_combinations(m: int, card_min: int, card_max: int) -> int:
-    if m < combinatorics(card_min, card_max):
-        m += 1
-    return m
+    # Add relations to features
 
 
-def lists(l: List, n: int, m: int, card_min:int, card_max: int) -> List:
-    result =[]
-    positives = list(itertools.combinations(l, iter_combinations(n, card_max)))  # n va desde card_min hasta card_max
-    negatives = set(l) - set(positives[set_combinations(m, card_min, card_max)])  # m va recorriendo los índices desde cero hasta combinatorics(card_min, card_max)
-    for node in l:
-        if node in positives:
-            result.append(Node(node))
-        elif node in negatives:
-            result.append(AST.create_unary_operation(ASTOperation.NOT, node).root)
-    return result
+    # Create the feature model
+    fm = FeatureModel(root=feature_root)
+    return fm
 
 
-def create_and_constraint_for_cardinality_group(positives: list[Feature], negatives: list[Feature]) -> AST:
+
+
+
+
+
+# def factorial(n: int) -> int:
+#     if n<0:
+#         raise Exception(f'"{n}" must be greater than 0.')
+#     return 1 if (n==1 or n==0) else n*factorial(n-1)
+
+# def combinatorics(card_min: int, card_max: int) -> int:
+#     return factorial(card_max)/(factorial(card_max - card_min)*factorial(card_min))
+
+# def iter_combinations(n: int, card_max: int) -> int:
+#     if n < card_max:
+#         n += 1
+#     return n
+
+# def set_combinations(m: int, card_min: int, card_max: int) -> int:
+#     if m < combinatorics(card_min, card_max):
+#         m += 1
+#     return m
+
+
+# def lists(l: List, n: int, m: int, card_min:int, card_max: int) -> List:
+#     result =[]
+#     positives = list(itertools.combinations(l, iter_combinations(n, card_max)))  # n va desde card_min hasta card_max
+#     negatives = set(l) - set(positives[set_combinations(m, card_min, card_max)])  # m va recorriendo los índices desde cero hasta combinatorics(card_min, card_max)
+#     for node in l:
+#         if node in positives:
+#             result.append(Node(node))
+#         elif node in negatives:
+#             result.append(AST.create_unary_operation(ASTOperation.NOT, node).root)
+#     return result
+
+
+def create_and_constraint_for_cardinality_group(positives: list[Feature], negatives: list[Feature]) -> Node:
     elements = [Node(f.name) for f in positives]
     elements += [AST.create_unary_operation(ASTOperation.NOT, Node(f.name)).root for f in negatives]
-    functools.reduce()
-    left = next((child for child in l), None)
-    l.pop(l.index(left))
-    right = each_tree(l, card_min, card_max)
-    return AST.create_binary_operation(ASTOperation.AND, left, right).root
+    return functools.reduce(lambda left, right: Node(ASTOperation.AND, left, right), elements)
 
 
 def get_or_constraints_for_cardinality_group(feature: Feature, relation: Relation) -> Node:
     card_min = relation.card_min
     card_max = relation.card_max
     children = set(relation.children)
+    and_nodes = []
     for k in range(card_min, card_max + 1):
+        print(f'K: {k}')
         combi_k = list(itertools.combinations(relation.children, k))
+        print(f'combi_k: {[str(f) for f in combi_k]}')
         for positives in combi_k:
+            print(f'Posities: {[str(f) for f in positives]}')
             negatives = children - set(positives)
+            print(f'Negatives: {[str(f) for f in negatives]}')
             and_ctc = create_and_constraint_for_cardinality_group(positives, negatives)
-
-    return AST.create_binary_operation(ASTOperation.OR,
-                                        create_and_constraint_for_cardinality_group(lists(feature.get_children(), card_min, card_max), card_min, card_max),
-                                        get_or_constraints_for_cardinality_group(feature, card_min, card_max)).root
-
+            print(f'Node: {and_ctc}')
+            and_nodes.append(and_ctc)
+            print('---')
+        print(f'Fin K {k}')
+    return functools.reduce(lambda left, right: Node(ASTOperation.OR, left, right), and_nodes)
 
 def get_constraint_for_cardinality_group(feature: Feature, relation: Relation) -> Constraint:
-    
-
-    or_ctc = get_or_constraints_for_cardinality_group(feature, relation)
-    return AST.create_binary_operation(ASTOperation.IMPLIES,
-                                        feature.name,
-                                        or_ctc).root
-
-
-
+    ast = AST.create_binary_operation(ASTOperation.IMPLIES,
+                                      Node(feature.name),
+                                      get_or_constraints_for_cardinality_group(feature, relation))
+    print(f'AST: {ast.pretty_str()}')
+    return Constraint('CG', ast)
 
 
 def get_new_feature_name(fm: FeatureModel, name: str) -> str:
@@ -201,6 +218,91 @@ def get_new_feature_name(fm: FeatureModel, name: str) -> str:
         new_name = f'{name}_p{count}'
         count += 1
     return new_name
+
+
+
+
+
+
+
+
+def transform_complex_crosstree_contraint(fm: FeatureModel, feature_name: str, ctc: Constraint) -> FeatureModel:
+    """Given a feature model, refactor the complex prop. log. cress-tree constraint."""
+    feature = fm.get_feature_by_name(feature_name)
+
+    if feature is None:
+        raise Exception(f'There is not feature with name "{feature_name}".')
+    if not feature.is_mutex_group():
+        raise Exception(f'Feature {feature_name} is not a mutex group.')
+    
+    new_name = get_new_feature_name(fm, feature_name)
+
+    f_p = Feature(name=new_name, parent=feature)
+
+    ctc_nodes = ctc.get_features()
+
+    r_mand = Relation(feature, [f_p], 1, 1)  # mandatory
+    r_or = Relation(f_p, [c for c in ctc_nodes], 1, len(ctc_nodes))  #or
+
+    ctc_nodes = ctc.get_features()
+    
+    for node in ctc_nodes:
+        node.parent = f_p
+
+    # Add relations to features
+    feature.add_relation(r_mand)
+    f_p.add_relation(r_or)
+    return fm
+
+
+
+
+
+
+def is_mult_group_decomposition(feature: Feature) -> bool:
+    is_mgd = False
+    suma = [r for r in feature.get_children() if feature.is_group()]
+    if len(suma)>1:
+        is_mgd = True
+    return is_mgd
+
+
+def new_decomposition(fm: FeatureModel, feature: Feature, r_group: Relation) -> FeatureModel:
+    new_name = get_new_feature_name(fm, feature.name)
+    f_p = Feature(name=new_name, parent=feature, is_abstract=True)
+    r_mand = Relation(feature, [f_p], 1, 1)  # mandatory
+    feature.add_relation(r_mand)
+    
+    r_group.parent = f_p
+
+    for child in r_group.children:
+        child.parent = f_p
+
+    # Add relations to features
+    f_p.add_relation(r_group)
+
+    feature.get_relations().remove(r_group)
+
+    return fm
+
+
+def transform_mult_group_decomposition(fm: FeatureModel, feature_name: str) -> FeatureModel:
+    """Given a feature model, refactor the multiple group decomposition specified by its name."""
+    feature = fm.get_feature_by_name(feature_name)
+
+    if feature is None:
+        raise Exception(f'There is not feature with name "{feature_name}".')
+    if not is_mult_group_decomposition(feature):
+        raise Exception(f'Feature {feature_name} is not a multiple group decomposition.')
+
+
+    relations = [r for r in feature.get_relations()]
+    for r in relations:
+        if r.is_group():
+            fm = new_decomposition(fm, feature, r)
+
+
+    return fm
 
 
 
@@ -255,58 +357,13 @@ def transform_cardinality(fm: FeatureModel, feature_name: str) -> FeatureModel:
     feature.get_relations().remove(r_card)
 
     constraint = get_constraint_for_cardinality_group(feature, r_card)
-
-    fm.ctcs = constraints
-
-    return fm
-
-
-
-
-def is_mult_group_decomposition(feature: Feature) -> bool:
-    is_mgd = False
-    suma = [r for r in feature.get_children() if feature.is_group()]
-    if len(suma)>1:
-        is_mgd = True
-    return is_mgd
-
-
-def new_decomposition(fm: FeatureModel, feature: Feature, r_group: Relation) -> FeatureModel:
-    new_name = get_new_feature_name(fm, feature.name)
-    f_p = Feature(name=new_name, parent=feature, is_abstract=True)
-    r_mand = Relation(feature, [f_p], 1, 1)  # mandatory
-    feature.add_relation(r_mand)
-    
-    r_group.parent = f_p
-
-    for child in r_group.children:
-        child.parent = f_p
-
-    # Add relations to features
-    f_p.add_relation(r_group)
-
-    feature.get_relations().remove(r_group)
+    print(f'Cosntraint: {constraint}')
+    fm.ctcs.append(constraint)
 
     return fm
 
 
-def transform_mult_group_decomposition(fm: FeatureModel, feature_name: str) -> FeatureModel:
-    """Given a feature model, refactor the multiple group decomposition specified by its name."""
-    feature = fm.get_feature_by_name(feature_name)
 
-    if feature is None:
-        raise Exception(f'There is not feature with name "{feature_name}".')
-    if not is_mult_group_decomposition(feature):
-        raise Exception(f'Feature {feature_name} is not a multiple group decomposition.')
-
-
-    relations = [r for r in feature.get_relations()]
-    for r in relations:
-        if r.is_group():
-            fm = new_decomposition(fm, feature, r)
-
-
-    return fm
 
 
 
